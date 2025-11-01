6efec43bec4471147df6a76a59206c10d96cb2b1
Hanumantuphade <hanumantuphade63@gmail.com>
2025-11-01 12:01:14 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: false, // ensures PostCSS usage
-  },
+  /* config options here */
 };
 
 export default nextConfig;
